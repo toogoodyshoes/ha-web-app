8e5ace0f77cf749d04da2ace701c445e4066ce1e
toogoodyshoes <lekhadenihar11@gmail.com>
2024-03-07 17:11:48 +0530

    Modified rds db instacne to db cluster

--- main.tf
+++ main.tf
@@ -199,58 +199,38 @@ resource "aws_vpc_security_group_ingress_rule" "db_sg" {
 }
 
 resource "aws_db_subnet_group" "aurora_wp_sng" {
-  name       = "Aurora-Wordpress"
+  name       = "aurora-wordpress"
   subnet_ids = [aws_subnet.data-a.id, aws_subnet.data-b.id]
 }
 
-resource "aws_db_instance" "wp_rds" {
+resource "aws_rds_cluster" "wp_rds" {
   engine                 = "aurora-mysql"
-  instance_class         = "db.t4g.medium"
-  multi_az               = true
+  engine_version         = "5.7.mysql_aurora.2.11.4"
+  availability_zones     = ["ap-south-1a", "ap-south-1c"]
   network_type           = "IPV4"
   db_subnet_group_name   = aws_db_subnet_group.aurora_wp_sng.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
   port                   = 3306
-  db_name                = "wordpress"
-  identifier             = "Wordpress-Workshop"
-  username               = "wpadmin"
-  password               = "wpadmin"
+  database_name          = "wordpress"
+  cluster_identifier     = "wordpress-workshop"
+  master_username        = "wpadmin"
+  master_password        = "wpadminat123"
 }
 
-resource "aws_security_group" "wp_cache_client_sg" {
-  name   = "WP Cache Client SG"
-  vpc_id = aws_vpc.ha-web-app.id
+resource "aws_rds_cluster_instance" "ws_rds_cluster_instance" {
+  cluster_identifier   = aws_rds_cluster.wp_rds.id
+  db_subnet_group_name = aws_db_subnet_group.aurora_wp_sng.name
+  engine               = "aurora-mysql"
+  engine_version       = "5.7.mysql_aurora.2.11.4"
+  instance_class       = "db.t3.small"
 }
 
-resource "aws_security_group" "wp_cache_sg" {
-  name   = "WP Cache Client SG"
-  vpc_id = aws_vpc.ha-web-app.id
-}
-
-resource "aws_vpc_security_group_ingress_rule" "wp_cache_sg" {
-  security_group_id = aws_security_group.wp_cache_sg.id
-
-  ip_protocol                  = "tcp"
-  from_port                    = 11211
-  to_port                      = 11211
-  referenced_security_group_id = aws_security_group.db_client_sg.id
-}
-
-resource "aws_elasticache_subnet_group" "wp-ec" {
-  name       = "Wordpress-Elasticache"
-  subnet_ids = [aws_subnet.data-a.id, aws_subnet.data-b.id]
-}
-
-resource "aws_elasticache_cluster" "wp-memcached" {
-  cluster_id           = "Wordpress-Memcached"
-  engine               = "memcached"
-  node_type            = "cache.t4g.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.memcached1.6"
-  port                 = 11211
-  network_type         = "ipv4"
-  subnet_group_name    = aws_elasticache_subnet_group.wp-ec.name
-  security_group_ids   = [aws_security_group.wp_cache_sg.id]
+resource "aws_rds_cluster_instance" "ws_rds_cluster_instance_reader" {
+  cluster_identifier   = aws_rds_cluster.wp_rds.id
+  db_subnet_group_name = aws_db_subnet_group.aurora_wp_sng.name
+  engine               = "aurora-mysql"
+  engine_version       = "5.7.mysql_aurora.2.11.4"
+  instance_class       = "db.t3.small"
 }
 
 resource "aws_security_group" "wp_fs_client_sg" {
@@ -266,7 +246,7 @@ resource "aws_security_group" "wp_fs_sg" {
 resource "aws_vpc_security_group_ingress_rule" "wp_fs_sg" {
   security_group_id = aws_security_group.wp_fs_sg.id
 
-  ip_protocol                  = "nfs"
+  ip_protocol                  = "tcp"
   from_port                    = 2049
   to_port                      = 2049
   referenced_security_group_id = aws_security_group.wp_fs_client_sg.id
@@ -355,7 +335,7 @@ resource "aws_efs_mount_target" "app-b-mt" {
 #   name                   = "Wordpress-LT"
 #   image_id               = "ami-0ba259e664698cbf"
 #   instance_type          = "t2.small"
-#   vpc_security_group_ids = [aws_security_group.wp_wordpress_sg.id, aws_security_group.db_client_sg.id, aws_security_group.wp_fs_client_sg.id, aws_security_group.wp_cache_client_sg.id]
+#   vpc_security_group_ids = [aws_security_group.wp_wordpress_sg.id, aws_security_group.db_client_sg.id, aws_security_group.wp_fs_client_sg.id]
 #   user_data              = filebase64("userdata.sh")
 # }
 
