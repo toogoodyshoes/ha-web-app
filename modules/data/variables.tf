81d04ad7af724b7131cdca54a36aebc10d9c02e9
toogoodyshoes <lekhadenihar11@gmail.com>
2024-03-08 17:38:58 +0530

    Changes
    1. Separated the module for data tier.

---
+++ modules/data/variables.tf
@@ -0,0 +1,19 @@
+variable "data_subnet_a_index" {
+    type = number
+    default = 2
+}
+
+variable "data_subnet_b_index" {
+    type = number
+    default = 2
+}
+
+variable "application_subnet_a_index" {
+    type = number
+    default = 1
+}
+
+variable "application_subnet_b_index" {
+    type = number
+    default = 1
+}
---
+++ modules/network/outputs.tf
@@ -0,0 +1,11 @@
+output "wp_vpc" {
+  value = aws_vpc.wordpress
+}
+
+output "zone_a_subnets" {
+  value = aws_subnet.zone_a_subnets
+}
+
+output "zone_b_subnets" {
+  value = aws_subnet.zone_b_subnets
+}
